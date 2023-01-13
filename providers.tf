469cc139bd9342559ad113ac2fada4f6e68d418b
Ishan Sharma <is@cloudeteer.de>
2023-01-13 03:10:39 +0100

    docs: added changelog, updated root readme and restrict with terraform versions

--- providers.tf
+++ providers.tf
@@ -4,5 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
+  required_version = ">= 1.0"
 }
