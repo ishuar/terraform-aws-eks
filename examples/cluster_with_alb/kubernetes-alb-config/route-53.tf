9bfcebfcc0b98b71822a579ff0b3d64006bd9a9c
Ishan Sharma <is@cloudeteer.de>
2023-01-13 01:53:34 +0100

    removed/renamed files for better names.

--- examples/cluster_with_alb/kubernetes-alb-config/route-53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_zone" "internet_alb" {
-  name = "worldofcontainers.tk"
-}
-
-## If hosting domain outside of AWS verify/add domain resolution is forwarded to following nameservers.
-output "zone_name_servers" {
-  value = aws_route53_zone.internet_alb.name_servers
-}
