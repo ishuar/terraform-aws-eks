ecd6aaf4f92db07885e19074e7ab119e386796d6
Ishan Sharma <is@cloudeteer.de>
2023-02-02 01:21:29 +0100

    example IRSA added

---
+++ examples/irsa/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+provider "kubernetes" {
+  host                   = module.irsa_eks.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.irsa_eks.eks_cluster_certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+    command     = "aws"
+  }
+}
+
