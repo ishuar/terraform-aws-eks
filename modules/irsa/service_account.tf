0db8b6bca1aec6fd030dddf795a240fb7ca8aa6e
Ishan Sharma <is@cloudeteer.de>
2023-02-03 01:17:31 +0100

    removed duplicate local block , similar condition is used for resource creation

--- modules/irsa/service_account.tf
+++ modules/irsa/service_account.tf
@@ -1,6 +1,5 @@
 locals {
   create_namespace_via_this_module = var.enable_irsa && var.create_kubernetes_namespace && var.namespace != "kube-system" && var.namespace != "default" ? 1 : 0
-  service_account_annotations      = var.enable_irsa && var.create_service_account ? merge({ "eks.amazonaws.com/role-arn" = aws_iam_role.this[0].arn }, var.service_account_annotations) : var.service_account_annotations
 }
 
 resource "kubernetes_namespace_v1" "this" {
@@ -24,6 +23,6 @@ resource "kubernetes_service_account_v1" "this" {
   metadata {
     name        = var.service_account_name
     namespace   = var.namespace
-    annotations = local.service_account_annotations
+    annotations = merge({ "eks.amazonaws.com/role-arn" = aws_iam_role.this[0].arn }, var.service_account_annotations)
   }
 }
